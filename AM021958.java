a562ece1c871577bb6fa8751aee431e4a440026c
JoeyChiou <joeyc0916@gmail.com>
2015-04-15 09:44:33 +0800

    Revert "commit send"
    This reverts commit 5c63ad819d67fc426e8d48bbaf4364679edca64f.

--- AM021958.java
+++ AM021958.java
@@ -2,6 +2,6 @@ public class AM021958{
     public static void main(String[] args) {
         System.out.println("AM021958 陳宥宏");
 	System.out.println("AM021953 張哲中");
-	System.out.println("AM021956 陳奕臣");
+	System.out.println("AM123456 1234568");
     }
 }
